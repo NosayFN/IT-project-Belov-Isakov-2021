6e9437c9f0bcbb4e59de67f56f79e98e422f2f40
NosayFN <kirbelov05@gmail.com>
2022-01-30 13:55:16 +0300

    firts commit

---
+++ app.py
@@ -0,0 +1,34 @@
+from flask import Flask, request
+import requests
+
+from utils import Config
+
+app = Flask(__name__)
+
+conf = Config.get_instance()
+
+
+@app.route('/')
+def hello_world():
+    return "Hello World! I am a test bot"
+
+
+@app.route('/message', methods=["GET", "POST"])
+def message():
+    print(conf.url)
+    if request.method == "POST":
+        print(request.json)
+        chat_id = request.json["message"]["chat"]["id"]
+        send_message(chat_id, "pong")
+    return {"ok": True}
+
+
+def send_message(chat_id, text):
+    method = "sendMessage"
+    url = conf.url + method
+    data = {"chat_id": chat_id, "text": text}
+    # requests.post(url, data=data)
+
+
+if __name__ == '__main__':
+    app.run()
