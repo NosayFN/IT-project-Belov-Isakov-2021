f2810560a97e499b67ee7eac6eab94116d66d2e9
NosayFN <kirbelov05@gmail.com>
2022-01-30 16:37:46 +0300

    Use env

--- utils/config.py
+++ utils/config.py
@@ -1,25 +1,20 @@
-import json
+import os
 
 
 class Config(object):
     # Global instance
     _instance = None
 
-    def __init__(self, **kwargs):
-        filename = kwargs.get('file')
-
-        with open(filename) as config_file:
-            config = json.loads(config_file.read())
-        self._url = config.get('url', None)
-
+    def __init__(self):
+        self._token = os.environ.get('TOKEN', None)
         Config._instance = self
 
     @classmethod
     def get_instance(cls):
         if not cls._instance:
-            cls._instance = Config(file='config.json')
+            cls._instance = Config()
         return cls._instance
 
     @property
     def url(self):
-        return self._url
+        return "https://api.telegram.org/bot" + self._token + "/"
